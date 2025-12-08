f8fe3d9e39bebc8b6b691c45f55da09f9304bb1f
Arshad Jamil <arshadjamil533@gmail.com>
2025-12-08 21:32:26 +0530

    added social meadia link and project links

--- script.js
+++ script.js
@@ -39,3 +39,56 @@ let navLinks = document.querySelectorAll(".menu li a");
 navLinks.forEach((link) => {
   link.addEventListener("click", hideNavMenu);
 });
+
+
+//chat with me code 
+const ChatBtn = ()=>{
+  document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("contactModal");
+  const chatBtn = document.getElementById("chatBtn");
+  const hireMeBtn = document.getElementById("hireMeBtn");
+  const closeBtn = modal.querySelector(".close");
+  const form = document.getElementById("contactForm");
+
+  chatBtn.onclick = () => modal.style.display = "flex";
+  hireMeBtn.onclick = () => modal.style.display = "flex"; // NEW
+  closeBtn.onclick = () => modal.style.display = "none";
+  window.onclick = (e) => { if (e.target == modal) modal.style.display = "none"; };
+
+  emailjs.init("Pqdtb2MJ6KtSleVuJ");
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    emailjs.sendForm("service_rg0e4lw", "template_kbg68zr", this)
+      .then(() => {
+        alert("Message sent successfully!");
+        form.reset();
+        modal.style.display = "none";
+      })
+      .catch((err) => {
+        console.error("EmailJS error:", err);
+        alert("Failed to send message.");
+      });
+  });
+});
+}
+
+ChatBtn();
+
+
+//menu socail links icon for mobile views
+
+const socialMenuBtn = ()=>{
+  const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.add('active');
+});
+
+cancelBtn.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+}
+
